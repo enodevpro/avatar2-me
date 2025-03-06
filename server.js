3cbb394d30d4905a12eca5502ff95cf6f8eae9e3
unknown <qthu.developer@gmail.com>
2025-03-06 16:04:16 +0700

    ednodev

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/db");
-const bodyParser = require("body-parser");
+
 const app = express();
 const port = process.env.PORT;
 
